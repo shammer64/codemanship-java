7268991a9598caaaeeba03b3bbe330f7bd0277ec
Scott Hammer <shammer@amerisure.com>
2021-11-19 17:53:49 -0500

    SH: added MethodSource for stream of integers

--- src/test/java/propertybased/SquareRootTest.java
+++ src/test/java/propertybased/SquareRootTest.java
@@ -1,7 +1,12 @@
 package propertybased;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,5 +16,15 @@ public class SquareRootTest {
     @CsvSource({"0,0", "1,1", "4,2", "9,3", "16,4", "25,5", "0.25,0.5", "2,1.414213562373095"})
     public void squareRootUsingBabylonianMethod(double operand, double root) {
         assertEquals(Maths.sqrt(operand), root, 0.0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parametersForSquareRootTest")
+    public void squareRootUsingBabylonianMethod_2(double operand, double root) {
+        assertEquals(Maths.sqrt(operand), root, 0.00000000001);
+    }
+
+    private static Stream<Arguments> parametersForSquareRootTest() {
+        return IntStream.range(0,100).mapToObj((num) -> Arguments.arguments(num, Math.sqrt(num)));
     }
 }
