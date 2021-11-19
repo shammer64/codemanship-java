2a6b13b4caa587075427e4faf591ba3109d31eed
Scott Hammer <shammer@amerisure.com>
2021-11-19 17:35:20 -0500

    SH: consolidated to ParametrizedTest; dropped Hamcrest assertion

--- src/test/java/propertybased/SquareRootTest.java
+++ src/test/java/propertybased/SquareRootTest.java
@@ -1,50 +1,15 @@
 package propertybased;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareRootTest {
 
-    @Test
-    public void sqrtOfZeroIsZero() {
-        assertThat(Maths.sqrt(0), is(0.0));
+    @ParameterizedTest
+    @CsvSource({"0,0", "1,1", "4,2", "9,3", "16,4", "25,5", "0.25,0.5", "2,1.414213562373095"})
+    public void squareRootUsingBabylonianMethod(double operand, double root) {
+        assertEquals(Maths.sqrt(operand), root, 0.0);
     }
-
-    @Test
-    public void sqrtOfOneIsOne() {
-        assertThat(Maths.sqrt(1), is(1.0));
-    }
-
-    @Test
-    public void sqrtOfOFourIsTwo() {
-        assertThat(Maths.sqrt(4), is(2.0));
-    }
-
-    @Test
-    public void sqrtOfONineIsThree() {
-        assertThat(Maths.sqrt(9), is(3.0));
-    }
-
-    @Test
-    public void sqrtOfOSixteenIsFour() {
-        assertThat(Maths.sqrt(16), is(4.0));
-    }
-
-    @Test
-    public void sqrtOfOTwentyFiveIsFive() {
-        assertThat(Maths.sqrt(25), is(5.0));
-    }
-
-    @Test
-    public void sqrtOfOneQuarterIsoneHalf() {
-        assertThat(Maths.sqrt(0.25), is(0.5));
-    }
-
-    @Test
-    public void sqrtOfTwo() {
-        assertThat(Maths.sqrt(2), is(1.414213562373095));
-    }
-
 }
