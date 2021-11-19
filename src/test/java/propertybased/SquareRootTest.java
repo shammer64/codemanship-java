4fd664be318c4e83c21a07daf92e1a4e1e70b1d3
Scott Hammer <shammer@amerisure.com>
2021-11-19 18:06:11 -0500

    SH: added MethodSource and test for stream of fractional doubles

--- src/test/java/propertybased/SquareRootTest.java
+++ src/test/java/propertybased/SquareRootTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -15,16 +16,33 @@ public class SquareRootTest {
     @ParameterizedTest
     @CsvSource({"0,0", "1,1", "4,2", "9,3", "16,4", "25,5", "0.25,0.5", "2,1.414213562373095"})
     public void squareRootUsingBabylonianMethod(double operand, double root) {
-        assertEquals(Maths.sqrt(operand), root, 0.0);
+        assertEquals(root, Maths.sqrt(operand), 0.0);
     }
 
     @ParameterizedTest
     @MethodSource("parametersForSquareRootTest")
     public void squareRootUsingBabylonianMethod_2(double operand, double root) {
-        assertEquals(Maths.sqrt(operand), root, 0.00000000001);
+        assertEquals(root, Maths.sqrt(operand), 0.00000000001);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parametersForSquareRootTest")
+    public void squareRootUsingBabylonianMethod_3(double operand, double root) {
+        assertEquals(operand, Maths.sqrt(operand) * Maths.sqrt(operand), 0.00000000001);
     }
 
     private static Stream<Arguments> parametersForSquareRootTest() {
         return IntStream.range(0,100).mapToObj((num) -> Arguments.arguments(num, Math.sqrt(num)));
     }
+
+    @ParameterizedTest
+    @MethodSource("parametersAsFractionsForSquareRootTest")
+    public void squareRootUsingBabylonianMethod_4(double operand) {
+        assertEquals(operand, Maths.sqrt(operand) * Maths.sqrt(operand), 0.00000000001);
+    }
+
+    private static Stream<Arguments> parametersAsFractionsForSquareRootTest() {
+        return IntStream.range(2, 50).mapToDouble((num) -> (double)1/num).mapToObj((fraction) -> Arguments.arguments(fraction));
+    }
+
 }
